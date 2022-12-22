7d9e7e5d880fc1f4bd3281cd15cd6ea3c43e7555
La-folia <kang0481@naver.com>
2022-12-22 17:16:10 +0900

    add Solution File [Bessel solution.py]

---
+++ example Bessel solution.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 22 17:11:44 2022
+
+@author: kang0
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+plt.rcParams.update({'font.size': 15})
+#%config InlineBackend.figure_format='retina'
+
+from scipy import special
+
+x = np.linspace(0, 30, 200)
+y = special.jn(0, x)
+
+plt.figure()
+plt.plot(x, y, '-r', label="$J_0(x)$")
+plt.xlabel("$x$")
+plt.ylabel("$J_0(x)$")
+plt.grid()
+plt.show()
