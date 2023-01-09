3946af350840292a89d8aa3675aa42b0379d3a00
La-folia <kang0481@naver.com>
2023-01-09 13:18:22 +0900

    latex edit 2022-1-9 13:18

--- Root_Finding/Fractals_Homework1.py
+++ Root_Finding/Fractals_Homework1.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
+# In[24]:
 
 
 import numpy as np
@@ -10,7 +10,7 @@ plt.rcParams.update({'font.size': 15})
 #%config InlineBackend.figure_format='retina'
 
 
-# In[15]:
+# In[43]:
 
 
 from scipy import optimize
@@ -21,24 +21,17 @@ def fprime(x):
     return 3*x**2
     
 
-sol = optimize.root_scalar(f, x0=0.2, fprime=fprime, method='newton')
- 
-    
-#    if abs(y) > 1e-10 or abs(x - 1) > 1e-10:
-#            return 0
-#        return 1
-
 def found_real_root(zinit: complex) -> int:
-    root = optimize.root_scalar(f, 
+    sol = optimize.root_scalar(f, 
                                 x0 = 1,
                                 fprime = fprime,
                                 method = 'newton')
-    x, y = root.real, root.imag
+    x, y = sol.root.real, sol.root.imag
+    print(x, y)
     if abs(y) > 1e-10 or abs(x - 1) > 1e-10:
         return 0
+
     return 1
-
-
 
 xs = np.linspace(-2, 2, 700)
 ys = xs
@@ -50,4 +43,19 @@ plt.imshow(mask, interpolation='none', origin='lower', cmap='Greys',
 plt.xlabel('$\\Re(z)$')
 plt.ylabel('$\\Im(z)$')
 plt.show()
+
+
+# In[26]:
+
+
+root = optimize.root_scalar(f, 
+                                x0 = 1j,
+                                fprime = fprime,
+                                method = 'newton')
+
+
+# In[28]:
+
+
+root.iterations
 
